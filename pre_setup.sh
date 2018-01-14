ea21eb66eb744559f1adc398816404fcaf80c93c
Carter Yagemann <yagemann@gatech.edu>
2018-01-14 13:12:31 -0500

    Add dkms setup to pre-setup script.

--- pre_setup.sh
+++ pre_setup.sh
@@ -11,3 +11,9 @@ else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	exec sudo "${DIR}/$(basename $0)"
 fi
+
+cp /var/lib/go-agent/pipelines/target-agent/test/dev/* /usr/src/spec-1.0
+
+if ! dkms status spec/1.0 | grep -q ^spec; then
+	dkms add -m spec -v 1.0
+fi
