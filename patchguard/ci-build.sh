02cf2da1e561da2f89c58bf750b05f119e2246cf
Wilson Martin <wilson.martin@gtri.gatech.edu>
2019-02-19 13:59:45 -0500

    update patchguard ci-build script to use the most up-to-date Module.symvers from the target-spec pipeline

--- patchguard/ci-build.sh
+++ patchguard/ci-build.sh
@@ -3,6 +3,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 patchguard_src="${DIR}/patchguard.c"
 PVERSION=$(grep ^MODULE_VERSION ${patchguard_src} | cut -d\" -f2)
+ARTIFACT=${DIR}/../ci-build-path.target-spec
+if [ -f $ARTIFACT ]; then
+    export KERNEL_DIR="$(cat ${ARTIFACT} | xargs)/spec_build"
+else
+    echo could not find \"$ARTIFACT\". module symbols will not match.
+fi
 /usr/bin/sudo -E /usr/sbin/dkms build -m patchguard -v $PVERSION -k 3.5.0-99-generic
 /usr/bin/sudo -E /usr/sbin/dkms mkdeb -m patchguard -v $PVERSION -k 3.5.0-99-generic
 /bin/cp /var/lib/dkms/patchguard/$PVERSION/deb/*.deb .
