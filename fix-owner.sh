c800960328e12e2d24248fca7b10d6a3b451f73a
Wilson Martin <wilson.martin@gtri.gatech.edu>
2019-02-20 11:45:18 -0500

    fix the fix-owner.sh script to not do anything if there is nothing to do

--- fix-owner.sh
+++ fix-owner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-find ${DIR} -user root -print0 | xargs -0 chown go:go
+find ${DIR} -user root -print0 | xargs -0 --no-run-if-empty chown go:go
