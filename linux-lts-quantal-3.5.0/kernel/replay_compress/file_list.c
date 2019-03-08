6c119208a60d0eeade6a7ec8c7e38319cfcab28a
Wilson Martin <wilson.martin@gtri.gatech.edu>
2019-03-08 17:15:51 -0500

    major update: change all functions used by record     and replay to swap the current user credentials     to root before any vfs operations.
        functions:
            add_file_to_cache()
            ensure_replayfs_paths()
            init_file_list()
            load_from_cache_file()
            make_logdir_for_replay_id()
            open_mmap_cache_file()
            read_user_extra_log()
            read_user_log()
            replay_checkpoint_to_disk()
            replayfs_diskalloc_read_page_location()
            replayfs_diskalloc_write_page_location()
            replay_resume_from_disk()
            save_to_cache_file()
            write_user_extra_log()
            write_user_log()

--- linux-lts-quantal-3.5.0/kernel/replay_compress/file_list.c
+++ linux-lts-quantal-3.5.0/kernel/replay_compress/file_list.c
@@ -24,16 +24,20 @@ int init_file_list (struct file_list_struct* file_list, char* file_list_path) {
 	loff_t pos = 0;
 	//change it later
 
+  THEIA_DECLARE_CREDS;
+  //swap creds to root for vfs operations
+  THEIA_SWAP_CREDS_TO_ROOT;
+
 	mutex_init (&file_list->file_list_mutex);
 	
 	file_list_path = (char*)REPLAYFS_FILELIST_PATH;
 
-	printk ("Pid %d begins init_file_list, filename %s\n", current->pid, file_list_path);
+	pr_debug ("Pid %d begins init_file_list, filename %s\n", current->pid, file_list_path);
 	set_fs (KERNEL_DS);
 
 	fd = sys_open (file_list_path, O_RDONLY, 0);
 	if (fd < 0) {
-		printk ("init_file_list , cannot open file %s, return %d\n", file_list_path, fd);
+		pr_err("init_file_list , cannot open file %s, return %d\n", file_list_path, fd);
 		goto exit;
 	}
 	file = fget (fd);
@@ -41,7 +45,7 @@ int init_file_list (struct file_list_struct* file_list, char* file_list_path) {
 	//read the count for ignored files
 	copyed = vfs_read (file, (char*) &count, sizeof(int), &pos);
 	if (copyed != sizeof (int)) {
-		printk ("init_file_list: ftried to read count for ignored_file_list, got rc %d\n", copyed);
+		pr_err("init_file_list: tried to read count for ignored_file_list, got rc %d\n", copyed);
 		rc = copyed;
 		goto exit;
 	}
@@ -50,13 +54,13 @@ int init_file_list (struct file_list_struct* file_list, char* file_list_path) {
 		file_list->ignored_count = count;
 		file_list->ignored_list = (struct file_list_name_struct*) kmalloc (count * sizeof(struct file_list_name_struct), GFP_KERNEL);
 		if (file_list->ignored_list == NULL) {
-			printk ("init_file_list: unable to allocate ignored_list with size %lu\n", count*sizeof(struct file_list_name_struct));
+			pr_err("init_file_list: unable to allocate ignored_list with size %lu\n", count*sizeof(struct file_list_name_struct));
 			rc = -ENOMEM;
 			goto exit;
 		}
 		copyed = vfs_read (file, (char*)file_list->ignored_list, count*sizeof(struct file_list_name_struct), &pos);
 		if (copyed != count*sizeof(struct file_list_name_struct)) {
-			printk ("init_file_list: ftried to read ignored_list for ignored_file_list, got rc %d, expected:%lu\n", copyed, count*sizeof(struct file_list_name_struct));
+			pr_err("init_file_list: ftried to read ignored_list for ignored_file_list, got rc %d, expected:%lu\n", copyed, count*sizeof(struct file_list_name_struct));
 			rc = copyed;
 			goto exit;
 		}
@@ -68,7 +72,7 @@ int init_file_list (struct file_list_struct* file_list, char* file_list_path) {
 	//read the count for modify files
 	copyed = vfs_read (file, (char*) &count, sizeof(int), &pos);
 	if (copyed != sizeof (int)) {
-		printk ("init_file_list: ftried to read count for modify_file_list, got rc %d\n", copyed);
+		pr_err("init_file_list: ftried to read count for modify_file_list, got rc %d\n", copyed);
 		rc = copyed;
 		goto exit;
 	}
@@ -77,13 +81,13 @@ int init_file_list (struct file_list_struct* file_list, char* file_list_path) {
 		file_list->modify_count = count;
 		file_list->modify_list = (struct file_list_name_struct*) kmalloc (count * sizeof(struct file_list_name_struct), GFP_KERNEL);
 		if (file_list->modify_list == NULL) {
-			printk ("init_file_list: unable to allocate modify_list with size %lu\n", count*sizeof(struct file_list_name_struct));
+			pr_err("init_file_list: unable to allocate modify_list with size %lu\n", count*sizeof(struct file_list_name_struct));
 			rc = -ENOMEM;
 			goto exit;
 		}
 		copyed = vfs_read (file, (char*)file_list->modify_list, count*sizeof(struct file_list_name_struct), &pos);
 		if (copyed != count*sizeof(struct file_list_name_struct)) {
-			printk ("init_file_list: ftried to read modify_list for modify_file_list, got rc %d, expected:%lu\n", copyed, count*sizeof(struct file_list_name_struct));
+			pr_err("init_file_list: ftried to read modify_list for modify_file_list, got rc %d, expected:%lu\n", copyed, count*sizeof(struct file_list_name_struct));
 			rc = copyed;
 			goto exit;
 		}
@@ -92,17 +96,16 @@ int init_file_list (struct file_list_struct* file_list, char* file_list_path) {
 		file_list->modify_list = NULL;
 	}
 
-
-
-	printk ("Pid %d init_file_list done.\n", current->pid);
+	pr_debug("Pid %d init_file_list done.\n", current->pid);
 
 exit:
 	if (fd >= 0) {
 		rc = sys_close (fd);
-		if (rc < 0) printk ("init_file_list: close returns %d\n", rc);
+		if (rc < 0) pr_err("init_file_list: close returns %d\n", rc);
 	}
 	if (file) fput(file);
 	set_fs(old_fs);
+  THEIA_RESTORE_CREDS;
 	if (rc < 0) return rc;
 	return 0;
 }
