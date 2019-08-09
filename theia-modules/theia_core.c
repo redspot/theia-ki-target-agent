6af9b9ba82c73e31b255f8fb02023173b69f74d0
Wilson Martin <wilson.martin@gtri.gatech.edu>
2019-08-09 17:49:39 -0400

    update theia_core module; change license, add init and exit

--- theia-modules/theia_core.c
+++ theia-modules/theia_core.c
@@ -1,8 +1,9 @@
 #include <linux/module.h>
+#include <linux/init.h>
 
 MODULE_DESCRIPTION("Theia core code and symbols");
 MODULE_AUTHOR("wilson.martin@gtri.gatech.edu");
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1-THEIA-0000");
 
 struct module* get_theia_core_module(void)
@@ -10,3 +11,16 @@ struct module* get_theia_core_module(void)
   return THIS_MODULE;
 }
 EXPORT_SYMBOL(get_theia_core_module);
+
+static int __init core_init(void)
+{
+  pr_info("theia_core loaded\n");
+  return 0;
+}
+module_init(core_init);
+
+static void __exit core_exit(void)
+{
+  pr_info("theia_core unloaded\n");
+}
+module_exit(core_exit);
